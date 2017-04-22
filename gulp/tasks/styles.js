d33a93bf593f99b84105e6f9ef332786c59d2ed1
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2017-04-22 23:32:28 +0200

    Barber pole mixin added, some workflow fixes

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,7 +4,7 @@ const sassModuleImporter = require('sass-module-importer');
 
 gulp.task('styles', function() {
     return gulp.src('./src/sass/**/*.scss')
-        .pipe(sass({ sassModuleImporter }))
+        .pipe(sass({ importer: sassModuleImporter() }))
         .on('error', function(errorInfo) {
             console.log(errorInfo.toString())
             this.emit('end');
